7b0863f70d2b0a7d8a91d0638e91686f0e73c61e
sam22unc <sam22@live.unc.edu>
2021-11-17 02:23:01 -0500

    delete message

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.patch("/app/update/user/:id", (req, res) => {
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id 
 app.delete("/app/delete/user/:id", (req, res) => {
 	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?").run([req.params.id]);
-	res.status(200).json({"message":'1 record deleted: ID ${req.params.id} (200)'});
+	res.status(200).json({"message":`1 record deleted: ID ${req.params.id} (200)`});
 });
 
 // Default response for any other request
