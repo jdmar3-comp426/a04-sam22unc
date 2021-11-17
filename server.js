6f8c873cea8c22cc16fd0f703d534f5d8f3c02f3
sam22unc <sam22@live.unc.edu>
2021-11-17 15:08:09 -0500

    update message

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get("/app/user/:id", (req, res) => {
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?").run([req.body.user],[md5(req.body.pass)],[req.params.id]);
-	res.status(200).json({"message":`1 record updated: ID ${req.params.id} (200)`},{"id":req.params.id,"user":req.body.user,"pass":md5(req.body.pass)});
+	res.status(200).json({"message":`1 record updated: ID ${req.params.id} (200)`});
 });
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id 
